7aa48d5207354c05d88077be39a1efb5654a89fd
VladimiraRe <vladimira_ra@mail.ru>
2023-01-03 22:15:28 +0300

    Refactor: make the variable distanceToNow immutable and therefore change the Task to a functional component

--- src/task/task.js
+++ src/task/task.js
@@ -1,62 +1,47 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { formatDistanceToNow } from 'date-fns';
 
 import Icon from '../icon';
 import './task.css';
 
-export default class Task extends Component {
-    static defaultProps = {
-        isDone: false,
-        description: '',
-        isEdit: false,
-        onDeleted: () => null,
-        onCompleted: () => null,
-        onEditing: () => null,
-        data: {},
-    };
+export default function Task({ data: { isDone, description, created }, isEdit, onDeleted, onEditing, onCompleted }) {
+    const distanceToNow = formatDistanceToNow(created, {
+        includeSeconds: true,
+    });
 
-    static propTypes = {
-        data: PropTypes.shape({
-            description: PropTypes.string,
-            created: PropTypes.number,
-            isDone: PropTypes.bool,
-        }),
-        isDone: PropTypes.bool,
-        description: PropTypes.string,
-        isEdit: PropTypes.bool,
-        onDeleted: PropTypes.func,
-        onCompleted: PropTypes.func,
-        onEditing: PropTypes.func,
-    };
-
-    state = {
-        created: formatDistanceToNow(this.props.data.created, {
-            includeSeconds: true,
-        }),
-    };
-
-    render() {
-        const {
-            data: { isDone, description },
-            isEdit,
-            onDeleted,
-            onEditing,
-            onCompleted,
-        } = this.props;
-        const { created } = this.state;
-        return (
-            <div className={`task${isEdit ? ' task--editing' : ''}`}>
-                <label>
-                    <input onChange={onCompleted} className='task__toggle' type='checkbox' checked={isDone} />
-                    <span className={`task__description${isDone ? ' task__description--done' : ''}`}>
-                        {description}
-                    </span>
-                </label>
-                <span className='task__created'>created {created}</span>
-                <Icon type='edit' onClick={onEditing} />
-                <Icon type='destroy' onClick={onDeleted} />
-            </div>
-        );
-    }
+    return (
+        <div className={`task${isEdit ? ' task--editing' : ''}`}>
+            <label>
+                <input onChange={onCompleted} className='task__toggle' type='checkbox' checked={isDone} />
+                <span className={`task__description${isDone ? ' task__description--done' : ''}`}>{description}</span>
+            </label>
+            <span className='task__created'>created {distanceToNow}</span>
+            <Icon type='edit' onClick={onEditing} />
+            <Icon type='destroy' onClick={onDeleted} />
+        </div>
+    );
 }
+
+Task.defaultProps = {
+    isDone: false,
+    description: '',
+    isEdit: false,
+    onDeleted: () => null,
+    onCompleted: () => null,
+    onEditing: () => null,
+    data: {},
+};
+
+Task.propTypes = {
+    data: PropTypes.shape({
+        description: PropTypes.string,
+        created: PropTypes.number,
+        isDone: PropTypes.bool,
+    }),
+    isDone: PropTypes.bool,
+    description: PropTypes.string,
+    isEdit: PropTypes.bool,
+    onDeleted: PropTypes.func,
+    onCompleted: PropTypes.func,
+    onEditing: PropTypes.func,
+};
